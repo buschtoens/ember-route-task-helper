a3fddb3070ce59e3ce432836da08037d5c0abc16
Jan Buschtöns <buschtoens@gmail.com>
2017-08-04 09:40:12 +0200

    chore: configure release pipeline

---
+++ config/release.js
@@ -0,0 +1,20 @@
+/* eslint-env node */
+const execSync = require('child_process').execSync;
+const generateChangelog = require('ember-cli-changelog/lib/tasks/release-with-changelog');
+
+module.exports = {
+  publish: true,
+  beforeCommit: generateChangelog,
+
+  afterPublish(project, versions) {
+    // Publish dummy app with docs to gh-pages
+    runCommand(`ember github-pages:commit --message "Released ${versions.next}"`);
+    runCommand('git push origin gh-pages:gh-pages');
+  }
+};
+
+function runCommand(command) {
+  console.log(`running: ${command}`);
+  const output = execSync(command, { encoding: 'utf8' });
+  console.log(output);
+}
