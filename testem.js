6c806323a4d18684e1563766427f50565dbc98a8
Jan Buschtöns <buschtoens@gmail.com>
2017-08-05 15:53:20 +0200

    chore(ci): use Chrome headless

--- testem.js
+++ testem.js
@@ -1,12 +1,19 @@
 /* eslint-env node */
+/* eslint-disable camelcase */
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: [
-    'PhantomJS'
-  ],
-  launch_in_dev: [
-    'PhantomJS',
-    'Chrome'
-  ]
+  launch_in_ci: ['Chrome'],
+  launch_in_dev: ['Chrome']
 };
+
+if (process.env.HEADLESS === 'true') {
+  module.exports.browser_args = {
+    Chrome: [
+      '--headless',
+      '--disable-gpu',
+      '--remote-debugging-port=9222',
+      '--window-size=1440,900'
+    ]
+  };
+}
