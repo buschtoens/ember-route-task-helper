5880ac4590cc389d5a79532e0bc9838180f3bc2a
Jan Buschtöns <buschtoens@gmail.com>
2017-08-05 15:40:48 +0200

    chore(deps): add and configure ember-suave and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,20 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
-  extends: 'eslint:recommended',
+  plugins: [
+    'prettier'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember-suave/recommended',
+    'prettier'
+  ],
   env: {
     browser: true
   },
   rules: {
+    'prettier/prettier': ['error', { singleQuote: true }],
+    'ember-suave/no-const-outside-module-scope': 'off',
+    'prefer-const': 'error'
   }
 };
