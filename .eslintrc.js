919993ce351854c105f7fc4dc63a454f76ec0829
Jan Buschtöns <jan@buschtoens.me>
2018-03-19 07:34:39 +0100

    chore: upgrade to ember-cli 3.0.1

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,12 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: [
+    'ember',
     'prettier'
   ],
   extends: [
     'eslint:recommended',
+    'plugin:ember/recommended',
     'plugin:ember-suave/recommended',
     'prettier'
   ],
@@ -20,5 +22,34 @@ module.exports = {
     'prettier/prettier': ['error', { singleQuote: true }],
     'ember-suave/no-const-outside-module-scope': 'off',
     'prefer-const': 'error'
-  }
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        'index.js',
+        'testem.js',
+        'ember-cli-build.js',
+        'config/**/*.js',
+        'tests/dummy/config/**/*.js'
+      ],
+      excludedFiles: [
+        'app/**',
+        'addon/**',
+        'tests/dummy/app/**'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      },
+      plugins: ['node'],
+      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+        // add your custom rules and overrides for node files here
+      })
+    }
+  ]
 };
