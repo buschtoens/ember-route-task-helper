0d6a8e197cf1bf69dcd10b0d12efd8409cfc6533
Jan Buschtöns <jan@buschtoens.me>
2019-02-15 10:30:56 +0100

    refactor: use router service instead of private API

--- addon/helpers/route-task.js
+++ addon/helpers/route-task.js
@@ -1,14 +1,12 @@
 import Helper from '@ember/component/helper';
-import { computed, get } from '@ember/object';
-import { getRouter } from 'ember-route-task-helper/utils/router';
-import { routeTaskFromRouter } from 'ember-route-task-helper/utils/route-task';
+import { get } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { routeTaskFromRouterService } from 'ember-route-task-helper/utils/route-task';
 
 export default Helper.extend({
-  router: computed(function() {
-    return getRouter(this);
-  }).readOnly(),
+  router: service(),
 
   compute([taskName, ...params]) {
-    return routeTaskFromRouter(get(this, 'router'), taskName, ...params);
+    return routeTaskFromRouterService(get(this, 'router'), taskName, ...params);
   }
 });
--- addon/utils/route-task.js
+++ addon/utils/route-task.js
@@ -1,16 +1,9 @@
-import Router from '@ember/routing/router';
+import { getOwner } from '@ember/application';
 import { assert } from '@ember/debug';
-import {
-  getRouter,
-  findTaskInCurrentRouteHierarchy
-} from 'ember-route-task-helper/utils/router';
+import { get } from '@ember/object';
+import { Task } from 'ember-concurrency/-task-property';
 
-export function routeTaskFromRouter(router, taskName, ...params) {
-  assert(
-    '[ember-route-task-helper] No router provided to `routeTaskFromRouter`',
-    router instanceof Router
-  );
-
+export function routeTaskFromRouterService(router, taskName, ...params) {
   const task = findTaskInCurrentRouteHierarchy(router, taskName);
   assert(`[ember-route-task-helper] Unable to find task ${taskName}`, task);
 
@@ -22,8 +15,26 @@ export function routeTaskFromRouter(router, taskName, ...params) {
 }
 
 export default function routeTask(context, taskName, ...params) {
-  const router = getRouter(context);
-  assert('[ember-route-task-helper] Unable to lookup router', router);
+  const router = getOwner(context).lookup('service:router');
+  assert('[ember-route-task-helper] Unable to lookup router service', router);
 
-  return routeTaskFromRouter(router, taskName, ...params);
+  return routeTaskFromRouterService(router, taskName, ...params);
+}
+
+export function findTaskInCurrentRouteHierarchy(router, taskName) {
+  const owner = getOwner(router);
+  let currentRoute = get(router, 'currentRoute');
+
+  while (currentRoute) {
+    const realRoute = owner.lookup(`route:${currentRoute.name}`);
+    const task = get(realRoute, taskName);
+
+    if (task instanceof Task) {
+      return task;
+    }
+
+    currentRoute = currentRoute.parent;
+  }
+
+  return null;
 }
